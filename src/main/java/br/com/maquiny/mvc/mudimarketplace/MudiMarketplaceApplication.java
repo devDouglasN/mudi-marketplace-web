c7eec4bd88de913adda08177dcd9dfbd91396add
Douglas <douglas_timaofiel@hotmail.com>
2023-05-11 17:45:58 -0300

    Project created and Maven imported

---
+++ src/main/java/br/com/maquiny/mvc/mudimarketplace/MudiMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.maquiny.mvc.mudimarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudiMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudiMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/maquiny/mvc/mudimarketplace/MudiMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maquiny.mvc.mudimarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
